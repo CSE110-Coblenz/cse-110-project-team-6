706d34fed726a91716bfb5bf9a280563c275d782
Jacob Lee <Jacob.J.Lee@outlook.com>
2025-11-01 15:19:21 -0700

    Add text to title container of main game view

--- src/screens/MainGameScreen/MainGameView.ts
+++ src/screens/MainGameScreen/MainGameView.ts
@@ -1,5 +1,6 @@
 import Konva from "konva";
 
+import { NAME } from "../../constants.ts";
 import { Color, View } from "../../types.ts";
 
 export class MainGameView extends View {
@@ -70,8 +71,17 @@ class Container {
 }
 
 class TitleContainer extends Container {
+    private text: Konva.Text;
+
     constructor(x: number, y: number, width: number, height: number) {
         super(x, y, width, height);
+
+        const group = this.getGroup();
+
+        this.text = new Konva.Text({ fontSize: 48, text: NAME });
+        this.text.x(group.width() / 2 - this.text.width() / 2);
+        this.text.y(group.height() / 2 - this.text.height() / 2);
+        group.add(this.text);
     }
 }
 
