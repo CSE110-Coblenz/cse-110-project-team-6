6c35b096995b5820adf5abf11ec35878c84deef3
Jacob Lee <72679601+JacobLee23@users.noreply.github.com>
2025-11-03 20:46:35 -0800

    Revert "Setting Screen Design"

--- src/main.ts
+++ src/main.ts
@@ -33,15 +33,6 @@ class Application implements ScreenSwitch {
             }
         );
 
-        // Scaling
-        const scaleX = window.innerWidth / STAGE_WIDTH;
-        const scaleY = window.innerHeight / STAGE_HEIGHT;
-        const scale = Math.min(scaleX, scaleY);
-
-        this.stage.width(STAGE_WIDTH * scale);
-        this.stage.height(STAGE_HEIGHT * scale);
-        this.stage.scale({ x: scale, y: scale });
-
         // Create layer to which to add screens
         this.layer = new Konva.Layer();
         this.stage.add(this.layer);
