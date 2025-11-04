33b3afabd8791240e03f455f87febe9d9b0b4aac
Jacob Lee <Jacob.J.Lee@outlook.com>
2025-11-04 10:12:20 -0800

    Implement isometric grid in main game view

--- src/constants.ts
+++ src/constants.ts
@@ -3,3 +3,7 @@ export const NAME = "GEOMETROPOLIS";
 // Stage dimensions
 export const STAGE_WIDTH = 1280;
 export const STAGE_HEIGHT = 720;
+
+// Grid cell dimensions
+export const CELL_WIDTH = 32;
+export const CELL_HEIGHT = 16;
--- src/screens/MainGameScreen/MainGameView.ts
+++ src/screens/MainGameScreen/MainGameView.ts
@@ -1,6 +1,7 @@
 import Konva from "konva";
 
-import { NAME } from "../../constants.ts";
+import { NAME, CELL_WIDTH, CELL_HEIGHT } from "../../constants.ts";
+import type { Point } from "../../types.ts";
 import { Color, View } from "../../types.ts";
 
 export class MainGameView extends View {
@@ -129,9 +130,64 @@ class InventoryContainer extends Container {
 }
 
 class GridContainer extends Container {
+    private grid: GridCell[][];
+
     constructor(x: number, y: number, width: number, height: number) {
         super(x, y, width, height);
+
+        const group = this.getGroup();
+
+        this.grid = [];
+        for (let i = 0; i < 2 * height / CELL_HEIGHT; ++i) {
+            let row: GridCell[] = [];
+            for (let j = 0; j < width / CELL_WIDTH - (i % 2); ++j) {
+                let cell = new GridCell(
+                    j * CELL_WIDTH + (i % 2) * CELL_WIDTH / 2,
+                    i * CELL_HEIGHT / 2,
+                    CELL_WIDTH,
+                    CELL_HEIGHT
+                );
+                group.add(cell.getGroup());
+                row.push(cell);
+            }
+            this.grid.push(row);
+        }
     }
+
+    getGrid(): GridCell[][] { return this.grid; }
+}
+
+class GridCell {
+    private group: Konva.Group;
+
+    private points: Point[];
+    private cell: Konva.Line;
+
+    constructor(x: number, y: number, width: number, height: number) {
+        this.group = new Konva.Group(
+            { x: x, y: y, width: width, height: height }
+        );
+
+        this.points = [
+            { x: width / 2, y: 0 },
+            { x: width, y: height / 2 },
+            { x: width / 2, y: height },
+            { x: 0, y: height / 2 }
+        ];
+        this.cell = new Konva.Line(
+            {
+                points: this.points.flatMap(p => [p.x, p.y]),
+                stroke: Color.LightBlue,
+                strokeWidth: 2,
+                closed: true
+            }
+        );
+        this.group.add(this.cell);
+    }
+
+    getGroup(): Konva.Group { return this.group; }
+    getPoints(): Point[] { return this.points; }
+    getCell(): Konva.Line { return this.cell; }
 }
 
 class BuildingsContainer extends Container {
