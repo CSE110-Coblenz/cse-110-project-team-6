a8e6f9fc71687863669b481dfaf86e47836bf961
Jacob Lee <Jacob.J.Lee@outlook.com>
2025-11-10 10:08:26 -0800

    Implement controller for menu bar of main game view

--- src/screens/MainGameScreen/MainGameController.ts
+++ src/screens/MainGameScreen/MainGameController.ts
@@ -1,5 +1,5 @@
 import { MainGameView } from "./MainGameView.ts";
-import { Controller } from "../../types.ts";
+import { Controller, MenuItem, ScreenType } from "../../types.ts";
 import type { ScreenSwitch } from "../../types.ts";
 
 export class MainGameController extends Controller {
@@ -9,7 +9,48 @@ export class MainGameController extends Controller {
         super(screenSwitch);
 
         this.view = new MainGameView();
+
+        const menuItems = this.view.getMenuItems();
+        menuItems.forEach(
+            (value, index, array) => {
+                const group = value.getGroup();
+                switch (value.getItem()) {
+                    case MenuItem.Information:
+                        group.addEventListener(
+                            "click", (e: Event) => { this.handleInformationClick(); }
+                        )
+                        break;
+                    case MenuItem.Settings:
+                        group.addEventListener(
+                            "click", (e: Event) => { this.handleSettingsClick(); }
+                        )
+                        break;
+                    case MenuItem.Exit:
+                        group.addEventListener(
+                            "click", (e: Event) => { this.handleExitClick(); }
+                        );
+                        break;
+                    default:
+                        throw new TypeError(value.getItem());
+                }
+            }
+        );
     }
 
     getView(): MainGameView { return this.view; }
+
+    handleInformationClick(): void {
+        const screenSwitch = this.getScreenSwitch();
+        // TODO: Switch to information screen
+    }
+
+    handleSettingsClick(): void {
+        const screenSwitch = this.getScreenSwitch();
+        screenSwitch.switchScreen({ type: ScreenType.Settings });
+    }
+
+    handleExitClick(): void {
+        const screenSwitch = this.getScreenSwitch();
+        screenSwitch.switchScreen({ type: ScreenType.Title });
+    }
 }
