0475e64155ec8c398b0d3f749b13cef689fd19d5
Jacob Lee <Jacob.J.Lee@outlook.com>
2025-11-29 15:54:31 -0800

    Update score upon construction dialog confirmation

--- src/screens/MainGameScreen/MainGameModel.ts
+++ src/screens/MainGameScreen/MainGameModel.ts
@@ -1,9 +1,19 @@
 export class MainGameModel {
+    private score: number = 0;
+
     private wood: InventoryItem;
     private stone: InventoryItem;
 
     private targetLength: number = 0;
     private targetWidth: number = 0;
+
+    static randomizeParameter(min: number = 1, max: number = 100): number {
+        return Math.ceil(Math.random() * (max - min) + min);
+    }
+
+    static logisticScore(
+        d: number, min: number = 0, max: number = 100, k: number = 25, d0: number = 0.25
+    ): number { return (d === 0) ? max : (min + (max - min) / (1 + Math.exp(k * (d - d0)))) }
 
     constructor(initial: number) {
         this.wood = new InventoryItem(initial);
@@ -14,13 +24,9 @@ export class MainGameModel {
 
     getStone(): InventoryItem { return this.stone; }
 
-    randomizeParameter(min: number = 1, max: number = 100): number {
-        return Math.ceil(Math.random() * (max - min) + min);
-    }
-
     generateTargets(): void {
-        this.targetLength = this.randomizeParameter();
-        this.targetWidth = this.randomizeParameter();
+        this.targetLength = MainGameModel.randomizeParameter();
+        this.targetWidth = MainGameModel.randomizeParameter();
     }
 
     getTargetLength(): number { return this.targetLength; }
@@ -30,6 +36,22 @@ export class MainGameModel {
     getTargetArea(): number { return this.targetLength * this.targetWidth; }
 
     getTargetPerimeter(): number { return 2 * (this.targetLength + this.targetWidth); }
+
+    getScore(): number { return this.score; }
+
+    incrementScore(score: number) { this.score += score; }
+
+    scoreInput(area: number, perimeter: number): number {
+        const targetArea = this.getTargetArea();
+        const scoreArea = MainGameModel.logisticScore((area - targetArea) / targetArea);
+
+        const targetPerimeter = this.getTargetPerimeter();
+        const scorePerimeter = MainGameModel.logisticScore(
+            (perimeter - targetPerimeter) / targetPerimeter
+        );
+
+        return Math.sqrt(scoreArea * scorePerimeter);
+    }
 }
 
 class InventoryItem {
@@ -41,9 +63,7 @@ class InventoryItem {
 
     get(): number { return this.quantity; }
 
-    add(quantity: number): void {
-        this.quantity += quantity;
-    }
+    add(quantity: number): void { this.quantity += quantity; }
 
     subtract(quantity: number): boolean {
         if (quantity > this.quantity) {
