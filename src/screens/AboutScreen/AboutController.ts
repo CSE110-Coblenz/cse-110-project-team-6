12fc9eb46e7e9877ad022248a3bb39db01982ca7
W1uSeven7 <552394206@qq.com>
2025-12-02 14:06:11 -0800

    Add navigation logic for About button on Title page and exit button on About page

--- src/screens/AboutScreen/AboutController.ts
+++ src/screens/AboutScreen/AboutController.ts
@@ -1,6 +1,7 @@
 import { AboutView } from "./AboutView.ts";
 import { Controller } from "../../types.ts";
 import type { ScreenSwitch } from "../../types.ts";
+import { ScreenType } from "../../types.ts";
 
 export class AboutController extends Controller {
     private view: AboutView;
@@ -10,9 +11,9 @@ export class AboutController extends Controller {
 
         this.view = new AboutView();
         
-        // Bind click event to close button
+        // Bind click event to close button - switch back to Title screen
         this.view.getCloseGroup().on("click tap", () => {
-            this.hide();
+            this.screenSwitch.switchScreen({ type: ScreenType.Title });
         });
     }
 
